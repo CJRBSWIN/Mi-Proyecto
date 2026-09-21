b59d3d83bed00ef0731c00521e920a542af64db9
CarlosJRB <cjrbswin@gmail.com>
2026-09-21 09:49:07 +0200

    Nuevos cambios, ejercicios de estadistica con python

--- filtro_de_spam.py
+++ filtro_de_spam.py
@@ -7,7 +7,7 @@ spam_words = ["oferta", "gratis", "dinero", "ganar", "clic"]
 # Calcular la cantidad de palabras spam en el correo
 cantidad_de_spam = sum([1 for word in correo if word in spam_words])
 
-if cantidad_de_spam > 5:
+if cantidad_de_spam > 2:
     print("¡Alerta! Este correo electrónico podría ser spam.")
 else:
     print("Este correo electrónico parece ser legitimo")
